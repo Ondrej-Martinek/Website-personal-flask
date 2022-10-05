ce68a9c4abf50304fd6dad9b449597ddd9f626e0
Ondrej-Martinek <martinek.ondrej@gmail.com>
2022-10-05 21:10:00 +0200

    template-button change to make url relative

--- app.py
+++ app.py
@@ -1,22 +1,22 @@
-from distutils.log import debug
-import flask
-from flask import request
+from flask import Flask, request, render_template, url_for
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
 
 json_table = {}
 
 @app.route("/", methods = ['GET', 'POST'])
 def index():
     if request.method == 'GET':
-        return flask.render_template("index.html")
+        return render_template("index.html")
     elif request.method == 'POST':
         data = request.data
         print(data)
-        return flask.render_template("index.html", data=data)
+        #render_template("index.html", data=data)
+        return url_for(index) 
+'''
+@app.route("/secondPage")
+def secondPage():
+'''
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
-'''abc'''
